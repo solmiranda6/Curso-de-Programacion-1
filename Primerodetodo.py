3fb106cbbf4a99caeb33e2044b4f4d300d36f558
Sol <solmmiranda6@gmail.com>
2023-06-03 11:24:41 -0300

    mi primer commit

---
+++ Primerodetodo.py
@@ -0,0 +1,75 @@
+"""
+Actividad 3: print ("Hola Mundo")
+"""
+"""
+Actividad 4: sumar dos numeros
+n1=int(input("Ingresar el primer valor "))
+n2=int(input("Ingresar el segundo valor "))
+resultado=n1+n2
+print("El resultado de la suma es: ",resultado)
+"""
+"""
+Actividad 5: producto de dos numeros
+n1=int(input("Ingresar el primer valor: "))
+n2=int(input("Ingresar el segunfo valor: "))
+resultado=n1*n2
+print ("El resultado del producto es: ", resultado)
+"""
+"""
+Actividad 6: Calcular el perimetro de un cuadrado
+lado1=float(input("Ingresar el valor del lado 1: "))
+suma=lado1*4
+print ("El perimetro del cuadrado es: ", suma)
+"""
+"""
+Actividad 7: mostrar suma, diferencia y producto
+n1=5
+n2=3
+suma=n1+n2
+print ("La suma es: ", suma)
+difer=n1-n2
+print ("La diferencia es: ", difer)
+prod=n1*n2
+print ("El producto es: ", prod)
+"""
+"""
+Actividad 8: Minimercado, articulos y precios
+cant1=int(input("Ingrese la cantidad vendida del articulo 1: "))
+pre1=float(input("Ingrese el precio de venta del articulo 1: "))
+cant2=int(input("Ingrese la cantidad vendida del articulo 2: "))
+pre2=float(input("Ingrese el precio de venta del articulo 2: "))
+cant3=int(input("Ingrese la cantidad vendida del articulo 3: "))
+pre3=float(input("Ingrese el precio de venta del artículo 3: "))
+imp1=cant1*pre1
+imp2=cant2*pre2
+imp3=cant3*pre3
+imptotal=imp1+imp2+imp3
+print ("El importe total en concepto de ventas del articulo 1 es: ", imp1)
+print ("El importe total en concepto de ventas del articulo 2 es: ", imp2)
+print ("El importe total en concepto de ventas del articulo 3 es: ", imp3)
+print ("El importa total en consideracion de los tres articulos es: ", imptotal)
+"""
+"""
+Actividad 9: Vehiculo, kilometros y litros
+km=float(input("Ingrese los km recorridos recorridos por el vehiculo: "))
+pre=float(input("Ingrese el precio del combustible por litro: "))
+kmL=float(input("Ingrese los km recorridos por litro: "))
+lit_consumidos=km/kmL
+imp_gastado=lit_consumidos*pre
+print ("La cantidad de litros consumidos son: ", lit_consumidos)
+print ("El importe gastado en combustible es: ", imp_gastado)
+"""
+"""
+Actividad 10: temperatura promedio
+temp1=float(input("Ingrese la temperatura tomada en horas de la mañana: "))
+temp2=float(input("Ingrese la temperatura tomada en horas de la tarde: "))
+temp3=float(input("Ingrese la temperatura tomada en horas de la noche: "))
+tprom=(temp1+temp2+temp3)/3
+print("La temperatura promedio por dia es: ",round(tprom,1))
+"""
+contr=int,str(input("Ingrese una contraseña: "))
+print (contr)
+
+
+
+
---
+++ segundaclase.py
@@ -0,0 +1,32 @@
+"""
+nombre=input("Ingrese el nombre del usuario: ")
+apellido=input("Ingrese el apellido del usuario: ")
+
+nombre=nombre.lower()
+apellido=apellido.lower()
+
+print (nombre+"_"+apellido+"@tdf.edu.ar")
+"""
+"""
+distancia=float(input("Distancia aproximada entre ciudades: "))
+velocidad=float(input("Velocidad aproximada del desplazamiento del vehiculo por hora: "))
+tiempo=(distancia*1)/velocidad
+print ("El tiempo que demora en llegar el vehiculo es: ",round(tiempo,2))
+"""
+"""
+nombre=input("Ingrese su nombre: ")
+apellido=input("Ingrese su apellido: ")
+dni=int(input("Ingrese su DNI: "))
+edad=int(input("Ingrese su edad: "))
+telefono=int(input("Ingrese su numero de telefono: "))
+ciudad=input("Ingrese la ciudad de nacimiento: ")
+print(" ")
+print ("Bienvenido/a",apellido.upper()+",",nombre,"al Curso de Diseño Web!")
+print("*************************************************************")
+print("Tus datos son: ")
+print("DNI: ",dni)
+print("EDAD: ",edad)
+print("TELEFONO: ",telefono)
+print("CIUDAD: ",ciudad.capitalize())
+print("*************************************************************")
+"""
